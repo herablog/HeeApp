c905acf1cff7ca497d5634cfeabc08d6654870f8
herablog <kz.route21@gmail.com>
2012-03-21 23:08:33 +0900

    Update.

--- templates/js/hee.js
+++ templates/js/hee.js
@@ -1,7 +1,25 @@
-(function(){
+// (function(){
+  var audio = new Audio("sound/he.mp3");
+    audio.load();
+    audio.autoplay = false;
   
-var socket = new io.Socket('localhost:5000');
-socket.connect();
-// socket.on();
+var socket = io.connect('http://127.0.0.1:3000');
+socket.on('connect', function() {
 
-})();
+    
+    socket.on('test', function () {
+      console.log('test !');
+    });
+    
+    var el = document.getElementById('btnHee');
+    el.addEventListener('click', function(){
+      socket.emit('hee');
+    }, false);
+    
+  });
+
+  
+
+
+// })();
+
